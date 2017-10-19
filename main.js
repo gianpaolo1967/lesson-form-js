15d36d13ff31d858acb5e8d0e893da549047efa4
Paolo <gianpaolo1967@gmail.com>
2017-10-20 01:36:40 +0300

    checking step by step

--- main.js
+++ main.js
@@ -3,70 +3,72 @@
 
 		var sex = $('input[name=sex]:checked').val();
 
-
-
-		if($('input[name=sex]:checked') & $('#agr').is(':checked') ) {
-			var success = true;
-		}
-
-		if($('input').eq(0).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(1).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(2).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(3).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(4).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(5).val() == '') {
-			alert('error');
-		}
-
-		if($('input').eq(6).val() == '') {
-			alert('error');
-		}
-
-		if($('select').val() == '') {
-			alert('error');
-
-		}
-
-
-
-
-
- 		if(success == true) {
- 			var form = {
-			firstName: $('#name').val(),
-			lastName: $('#lName').val(), 
-			age: $('#age').val(),
-			sex: sex,
-			born: $('#date').val(),
-			withMe: $('#members').val(),
-			region: $('#state').val(),
-			phone: $('#phone').val(),
-			email: $('#email').val(),
-	}
-
-	 console.log(form);
-
- 			}
-
 		
 
+		if($('input[name=sex]:checked') && $('#agr').is(':checked')) {
+			if($('input').eq(0).val() !== '') {
+				if($('input').eq(1).val() !== '') {
+					if($('input').eq(2).val() !== '') {
+						if($('input').eq(3).val() !== '') {
+							if($('input').eq(4).val() !== '') {
+								if($('input').eq(5).val() !== '') {
+									if($('input').eq(6).val() !== '') {
+										if($('select').val() !== '') {
+											var form = {
+														firstName: $('#name').val(),
+														lastName: $('#lName').val(), 
+														age: $('#age').val(),
+														sex: sex,
+														born: $('#date').val(),
+														withMe: $('#members').val(),
+														region: $('#state').val(),
+														phone: $('#phone').val(),
+														email: $('#email').val(),
+												}
 
-	});
+												 console.log(form);
+
+									}
+										else {
+											alert('Fill all feeds');
+										}
+
+								}
+									else {
+											alert('Fill all feeds');
+									}
+
+							}
+								else {
+										alert('Fill all feeds');
+								}
+
+							}
+								else {
+										alert('Fill all feeds');
+								}
+						}
+							else {
+									alert('Fill all feeds');
+							}
+					}
+						else {
+								alert('Fill all feeds');
+						}
+				}
+					else {
+							alert('Fill all feeds');
+					}
+			}
+				else {
+						alert('Fill all feeds');
+				}
+		}
+			else {
+					alert('Fill all feeds');
+			}
+
+});
 
 	
 
